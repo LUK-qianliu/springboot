5edd7c74ef1bf1f679c4a5ad2f90e18b941fb08a
liu <532433365@qq.com>
2019-05-13 15:56:00 +0800

    feat:新增了test4，使用druid
    使用druid数据库连接池访问mysql，设置拦截器，在springboot内使用druid的一个网页管理器（servlet）来查看druid管理的数据库信息

---
+++ test4_springboot_Druid/src/main/java/com/qianliu/springboot_test/DruidConfiguration.java
@@ -0,0 +1,48 @@
+package com.qianliu.springboot_test;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration表示该类 类似于一个spring的xml文件，里面通常是一个些bean
+@Configuration
+public class DruidConfiguration {
+
+    /*
+     * @author qianliu on 2019/5/13 15:35
+     * @param
+     * @return ServletRegistrationBean 这是一个springboot的Servlet使用的注册器，这样可以在springboot中使用servlet
+     * @Discription:
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        //创建servlet注册实体
+        // 1.StatViewServlet是druid实现的一个Servlet容器，来显示druid的一些信息
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        //设置ip白名单
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+        //设置ip黑名单，如果allow与deny共同存在时,deny优先于allow
+        servletRegistrationBean.addInitParameter("deny","192.168.0.19");
+        //设置控制台管理用户
+        servletRegistrationBean.addInitParameter("loginUsername","druid");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        //是否可以重置数据
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    //设置网站的过滤信息
+    @Bean
+    public FilterRegistrationBean statFilter(){
+        //创建过滤器
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //设置过滤器过滤路径
+        filterRegistrationBean.addUrlPatterns("/*");
+        //忽略过滤的形式
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
---
+++ test4_springboot_Druid/src/main/java/com/qianliu/springboot_test/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.qianliu.springboot_test.controller;
+
+import com.qianliu.springboot_test.entity.UserEntity;
+import com.qianliu.springboot_test.jpa.UserJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    UserJPA userJPA;
+
+    /*
+     * @author qianliu on 2019/5/12 21:36
+     * @return List<UserEntity> 返回UserEntity实体的list<UserEntity>
+     * @Discription:查询所有数据
+     */
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public List<UserEntity> getAll(){
+        return userJPA.findAll();
+    }
+
+    /*
+     * @author qianliu on 2019/5/12 21:37
+     * @param entity 传入一个对象UserEntity
+     * @return UserEntity 返回这个UserEntity实体
+     * @Discription:更新、添加UserEntity实体
+     */
+    @RequestMapping(value = "/save",method = RequestMethod.GET)
+    public UserEntity save()
+    {
+        UserEntity entity = new UserEntity("user1",22,"123@qq.com");
+        return userJPA.save(entity);
+    }
+
+    /**
+     * 通过id删除用户方法
+     * @return
+     */
+    @RequestMapping(value = "/delete/{id}",method = RequestMethod.GET)
+    public List<UserEntity> delete(@PathVariable Long id)
+    {
+        userJPA.deleteById(id);
+        return userJPA.findAll();
+    }
+}
